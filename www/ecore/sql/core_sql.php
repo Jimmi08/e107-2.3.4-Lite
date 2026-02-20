1a7b2cfeeae9b252eb92c835426ffd0f9462a75c
Jimako <janamicek@gmail.com>
2026-02-20 10:32:51 +0100

    removed comments table

--- www/ecore/sql/core_sql.php
+++ www/ecore/sql/core_sql.php
@@ -86,31 +86,6 @@ CREATE TABLE banlist (
 # --------------------------------------------------------
 
 #
-# Table structure for table `comments`
-#
-
-CREATE TABLE comments (
-  comment_id int(10) unsigned NOT NULL auto_increment,
-  comment_pid int(10) unsigned NOT NULL default '0',
-  comment_item_id int(10) unsigned NOT NULL default '0',
-  comment_subject varchar(100) NOT NULL default '',
-  comment_author_id int(10) unsigned NOT NULL default '0',
-  comment_author_name varchar(100) NOT NULL default '',
-  comment_author_email varchar(200) NOT NULL default '',
-  comment_datestamp int(10) unsigned NOT NULL default '0',
-  comment_comment text NOT NULL,
-  comment_blocked tinyint(3) unsigned NOT NULL default '0',
-  comment_ip varchar(45) NOT NULL default '',
-  comment_type varchar(20) NOT NULL default '0',
-  comment_lock tinyint(1) unsigned NOT NULL default '0',
-  comment_share tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (comment_id),
-  KEY comment_blocked (comment_blocked),
-  KEY comment_author_id (comment_author_id) 
-) ENGINE=InnoDB;
-# --------------------------------------------------------
-
-#
 # Table structure for table `core`
 #
 CREATE TABLE core (
